125e182245e57e105ad7fc7cb0d2ab36781f40ce
Necobgs <emanuelborgesdev@gmail.com>
2025-08-21 20:20:56 -0300

    removed unused imports

--- src/components/page/user-page/CardUser.tsx
+++ src/components/page/user-page/CardUser.tsx
@@ -1,14 +1,11 @@
-import { Button } from "@/components/ui/button"
 import {
   Card,
-  CardAction,
   CardContent,
   CardDescription,
   CardFooter,
   CardHeader,
   CardTitle,
 } from "@/components/ui/card"
-import { Input } from "@/components/ui/input"
 import { Label } from "@/components/ui/label"
 import { iUser } from "@/types/user"
 
